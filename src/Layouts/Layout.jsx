74de63d8ea7925a6d26dee4bf954dc7005dad1e7
wasan <wasanawwad2020@gmail.com>
2024-01-24 21:20:54 +0200

    Create Navbar , Footer Component

--- src/Layouts/Layout.jsx
+++ src/Layouts/Layout.jsx
@@ -1,7 +1,12 @@
 import React from 'react'
+import Navbar from '../component/Navbar/Navbar'
+import Footer from '../component/Footer/Footer'
 
 export default function Layout() {
   return (
-    <div>Layout</div>
+   <>
+   <Navbar/>
+   <Footer/>
+   </>
   )
 }
