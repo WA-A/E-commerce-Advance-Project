3aeda2b4a11a1eb08777a13b4d5f4951e355d0c7
wasan <wasanawwad2020@gmail.com>
2024-01-25 12:41:39 +0200

    Create Categories for user

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,22 @@ import React from 'react'
 import {createBrowserRouter,RouterProvider,} from "react-router-dom";
 import Layout from './Layouts/Layout.jsx';
 import Home from './component/web/Home/Home.jsx';
+import Categories from './component/web/Categories/Categories.jsx';
 
 const router = createBrowserRouter([
   {
     path:'/',
     element:<Layout/>,
 
-    children:[
+    children:[    // any children see parent
      {
       path:'/home',
       element:<Home/>,
-     }
+     },
+     {
+      path:'/categories',
+      element:<Categories/>,
+     },
     ]
   }
 ]);
--- src/Layouts/Layout.jsx
+++ src/Layouts/Layout.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Navbar from '../component/web/Navbar/Navbar'
 import Footer from '../component/web/Footer/Footer'
+import { Outlet } from 'react-router-dom'
 
 export default function Layout() {   // Here are the fixed parts of the admin, even when it changes, I change it once
   return (
    <>
    <Navbar/>
+   <Outlet/>  {/*see the component that changes */}
    <Footer/>
    </>
   )
