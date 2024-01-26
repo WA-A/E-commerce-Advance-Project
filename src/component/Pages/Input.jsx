f245529b175946003935b67e5b718ed997aae56a
wasan <wasanawwad2020@gmail.com>
2024-01-26 22:49:01 +0200

    Handel Form using formik

--- src/component/Pages/Input.jsx
+++ src/component/Pages/Input.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 
-export default function Input({type='text',id,name,title}) {
+export default function Input({type='text',id,name,title,value,onChange}) {
   return (
     <>
     <div className='input-group mb-3'>
         <label htmlFor={id}>{title}</label>
-        <input type={type} name={name} className='form-control' id={id}/>
+        <input type={type} name={name} className='form-control' value={value} id={id} onChange={onChange}/>
     </div>
     </>
   )
--- src/component/web/Register/Register.jsx
+++ src/component/web/Register/Register.jsx
@@ -1,31 +1,47 @@
 import React from 'react'
 import Input from './../../Pages/Input';
+import {useFormik } from 'formik';
 
 export default function Register() {
+
+     const formik = useFormik(
+      {
+        initialValues:{   // use value for name from inputsRequired
+          userName:'',
+          email:'',
+          password:'',
+         
+        }
+      });
+      
+     console.log(formik.values);
 
   const inputsRequired =[
     {
       id:'username',
       type:'text',
       name:'userName',
-      title:'User Name'
+      title:'User Name',
+      value:formik.values.userName,  //Means I take the value and store it in formik
     },
     {
       id:'email',
       type:'email',
       name:'email',
-      title:'User Email'
+      title:'User Email',
+      value:formik.values.email,
     },
     {
       id:'password',
       type:'password',
       name:'password',
-      title:'User Password'
+      title:'User Password',
+      value:formik.values.password,
     },
   ];
 
   const renderinputsRequired = inputsRequired.map( (input,index)=>
-      <Input type={input.type} id={input.id} name={input.name} title={input.title} key={index}/>
+      <Input type={input.type} id={input.id} name={input.name} title={input.title} value={input.value} key={index} onChange={formik.handleChange}/>
   )
 
   return (
