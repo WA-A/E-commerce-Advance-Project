9cbe2499a66423ab2ceb06bb4c9f182b22160fd1
wasan <wasanawwad2020@gmail.com>
2024-01-26 20:38:00 +0200

    Inputs Required

--- src/component/web/Register/Register.jsx
+++ src/component/web/Register/Register.jsx
@@ -1,7 +1,31 @@
 import React from 'react'
 
 export default function Register() {
+
+  const inputsRequired =[
+    {
+      id:'username',
+      type:'text',
+      name:'userName',
+      title:'User Name'
+    },
+    {
+      id:'email',
+      type:'email',
+      name:'email',
+      title:'User Email'
+    },
+    {
+      id:'password',
+      type:'password',
+      name:'password',
+      title:'User Password'
+    },
+  ];
+
   return (
-    <div>Register</div>
+    <>
+    <h2> Create Account</h2>
+    </>
   )
 }
