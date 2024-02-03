228f38a11ee4d60c494dc22c92a506e62342bec4
wasan <wasanawwad2020@gmail.com>
2024-02-03 20:58:06 +0200

    Logged User

--- src/component/web/Context/User.jsx
+++ src/component/web/Context/User.jsx
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import { createContext, useState } from "react";
 
  export let  UserContex = createContext ();
@@ -5,8 +6,26 @@ import { createContext, useState } from "react";
  export  function UserContexProvider({children}){
 
     const [userToken,setUserToken]=useState(null);
+    const [userData,setUserData]=useState(null);
+     
+     const GetUserData = async()=>{
+        if(userToken){
+            const {data} = await axios.get("https://ecommerce-node4.vercel.app/user/profile",   // After Login must show profile user
+            {headers:{Authorization:`Tariq__${token}`}} 
+           
+            )
+            console.log(data);
+            setUserData(data.user);
+        }
+     }
 
-    return <UserContex.Provider value={{userToken,setUserToken}} >
+     useEffect( ()=>{
+        GetUserData();
+     },[userToken])
+
+
+
+    return <UserContex.Provider value={{userToken,setUserToken,userData,setUserData}} >
        {children}
     </UserContex.Provider>
  }
